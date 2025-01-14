583a4ce98f70aa9ff97ab620c9e2e59b1f3e1621
ruslan <leshchinskyruslan@gmail.com>
2025-01-14 18:52:02 +0300

    fix 1.1

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -2,14 +2,12 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import web.dao.CarsDAO;
 import web.models.Car;
 
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
 public class CarServiceImpl implements CarService {
 
     private final CarsDAO carsDAO;
